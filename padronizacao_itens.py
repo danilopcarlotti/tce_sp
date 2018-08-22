e4cb9d879f353747959b3c05b7ef6bb03721fe95
danilo_carlotti <danilopcarlotti@gmail.com>
2018-08-22 08:56:12 -0300

    criação de arquivo para padronização de itens com base em catálogo da BEC

--- padronizacao_itens.py
+++ padronizacao_itens.py
@@ -1,5 +1,6 @@
+from datetime import datetime
 from mNB_classification_text import mNB_classification_text
-import pickle, difflib, pandas as pd
+import pickle, difflib, pandas as pd, jellyfish, re
 
 class padronizacao_itens():
 	
@@ -10,7 +11,7 @@ class padronizacao_itens():
 		dados = []
 		for k, v in dicionario_itens.items():
 			for i in v:
-				dados.append((k,i))
+				dados.append((i,k))
 		sck = mNB_classification_text(dados)
 		pickle.dump(sck, open('classificador_%s.pickle' % (nome_dicionario,),'wb'))
 
@@ -64,21 +65,64 @@ class padronizacao_itens():
 		pickle.dump(self.dicionario_itens, open("dicionario_grupos_itens.pickle", "wb" ) )
 
 	def ratio_strings(self, string1, string2):	
-		diff = difflib.SequenceMatcher(lambda x: x==" ", string1, string2)
-		return diff.ratio()
+		# diff = difflib.SequenceMatcher(lambda x: x==" ", string1, string2)
+		# return diff.ratio()
+		return jellyfish.jaro_distance(string1, string2)
 
 
 def main():
 	p = padronizacao_itens()
-	# dic = pickle.load(open('/home/danilo/Documents/tce_sp/dicionario_itens.pickle','rb'))
-	# p.dicionario_desc_itens_diff(dic)
+	# dic_classes = pickle.load(open('/home/danilo/Documents/tce_sp/dicionario_classe_itens.pickle','rb'))
+	dic_grupos = pickle.load(open('/home/danilo/Documents/tce_sp/dicionario_grupos_itens.pickle','rb'))
+	
+	# CLASSIFICADORES E CLASSIFICAÇÃO
+	# classificador = pickle.load(open('/home/danilo/Documents/tce_sp/classificador_classes_itensX.pickle','rb'))
+	# classificador = pickle.load(open('/home/danilo/Documents/tce_sp/classificador_grupos_itensX.pickle','rb'))
+	# for c in classificadores_classes:
+	# 	classificador = pickle.load(open(c,'rb'))
+	# 	classe_encontrada = classificador.predict_mNB([STRING_ANALISADA])
+	# 	print('Valor parcial')
+	# 	print(classe_encontrada)
+	# 	for item in dic_classes[classe_encontrada[0]]:
+	# 		ratio_s = p.ratio_strings(STRING_ANALISADA,item)
+	# 		if ratio_s > maior_semelhanca:
+	# 			maior_semelhanca = ratio_s
+	# 			item_normalizado = item
+	# 			print(maior_semelhanca)
+	# 			print(item)
+	# print('Valor final')
+	# print(maior_semelhanca)
+	# print(item)
+
+	# DESCONSIDERANDO O CLASSIFICADOR mNB
+	def encontra_string_semelhante(dicionarioComparacao, stringAnalisada, nCaracteres):
+		maior_semelhanca = 0
+		item_normalizado = ''
+		if nCaracteres:
+			for k,v in dicionarioComparacao.items():
+				for item in v:
+					ratio_s = p.ratio_strings(STRING_ANALISADA[:nCaracteres],item[:nCaracteres])
+					if ratio_s > maior_semelhanca:
+						maior_semelhanca = ratio_s
+						item_normalizado = item
+			if maior_semelhanca > 0.8:
+				return ([maior_semelhanca, item_normalizado])		
+			else:
+				return encontra_string_semelhante(dicionarioComparacao, stringAnalisada, nCaracteres - 5)
+		else:
+			return 'Não encontrado'
+
+	STRING_ANALISADA = 'PALHA AÇO MÉDIA N. 1 EMBALAGEM PLÁSTICA COM 25GR.'
+	print(encontra_string_semelhante(dic_grupos, STRING_ANALISADA, 20))
+
+	# CRIAR DICIONÁRIO DE ITENS
 	# p.dicionario_classes_itens_criar('/home/danilo/Downloads/BASE 2009 - 2017 SEM QTDE FORNECEDORES PARTICIPANTES.csv')
 	# p.dicionario_grupos_itens_criar('/home/danilo/Downloads/BASE 2009 - 2017 SEM QTDE FORNECEDORES PARTICIPANTES.csv')
 	# p.dicionario_itens_criar('/home/danilo/Downloads/BASE 2009 - 2017 SEM QTDE FORNECEDORES PARTICIPANTES.csv')
-	dic_classes = pickle.load(open('/home/danilo/Documents/tce_sp/dicionario_classe_itens.pickle','rb'))
-	p.classificador_itens_dicionario(dic_classes,'classes_itens')
-	dic_grupos = pickle.load(open('/home/danilo/Documents/tce_sp/dicionario_grupos_itens.pickle','rb'))
-	p.classificador_itens_dicionario(dic_grupos,'grupos_itens')
+	# p.classificador_itens_dicionario(dic_classes,'classes_itens')
+	# p.classificador_itens_dicionario(dic_grupos,'grupos_itens')
+
+	# ENCONTRANDO TERMOS FREQUENTES A SEREM DESCONSIDERADOS?
 
 if __name__ == '__main__':
 	main()
